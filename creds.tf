d3ad0cd3cc07ce462ccf14224b31a89b9a7c9b88
Raja Thumati <rajaramthumati@gmail.com>
2020-05-05 19:55:54 +0000

    creds

---
+++ creds.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+default = "AKIAICT3OG4G2IUCAJMA"
+}
+
+variable "aws_secret_key" {
+default = "3doCEAXzlggBJZekeMAowLbQS+/tJcjy62MC1bdF"
+}
+
+variable "aws_region" {
+default = "us-east-2"
+}
